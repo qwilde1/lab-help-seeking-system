36b2fea37f8af5a9c46b348fa8cd1207d955e39d
tasmine hackson <thackson@linux60408.dc.engr.scu.edu>
2017-11-12 19:33:59 -0800

    adding more styling

--- taSession.php
+++ taSession.php
@@ -12,10 +12,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
 ?>
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="./styles/sessionStyle.css">
 		<script type="text/javascript">
 			function reload()
 			{
@@ -37,14 +37,11 @@ echo "Connected successfully";
 		</script>
 	</head>
 	<body onload="init()">
-		</br><a align=right href="taDashboard.php">Return to Dashboard page</a>
-		<h1>Hello <?php echo $_SESSION["username"] ?></h1>
-		<h2>Session: <?php echo $_SESSION["sessionName"] ?></h2>
-
-		<form method="post" action="taSession.php">
-			<textarea rows="5" cols="60" id="announcementData" name="announcementData" placeholder="Insert Announcement"></textarea></br>
-			<input type="submit" name="Submit" value="Announce"/>
-		</form>
+		<div class="top-bar">
+			<h1>Hello <?php echo $_SESSION["username"] ?></h1>
+			<h2>Session: <?php echo $_SESSION["sessionName"] ?></h2>
+			<a class="backButton" align=right href="taDashboard.php">Return to Dashboard page</a>
+		</div>
 
 		<?php
 			if(isset($_POST['resolveID']) && $_POST['resolveID'] != "")
@@ -56,9 +53,7 @@ echo "Connected successfully";
 			{
 				insertAnnouncement();
 			}
-			echo "<h2>Announcements</h2>";
 			echo displayAnnouncements();
-			echo "<h2>Questions</h2>";
 			echo displayQuestions();
 			
 			function updateEntry(){
@@ -66,45 +61,37 @@ echo "Connected successfully";
 				$quickcheck = $conn->prepare("UPDATE questions SET resolved = 1 where questionId= ?");
 				$quickcheck->bind_param("s", $questionId);
 				$questionId = $_POST["resolveID"];
-				if($quickcheck->execute()){
-					echo "</br>question resolved";
-				}
 				
 			}		
 
 			//Call this in a php block in the HTML for the page where you want the questions to appear
 			function displayQuestions(){
 				global $conn;
-				$tableHTML = "<div class=\"queue\" style=\"padding:20px\">
-						<table border=\"1\">
-							<tr>
-								<th>Student</th>
-								<th>Question</th>
-								<th>Resolve</th>
-							</tr>";
+				$tableHTML = "<div class=\"queue\" >
+						<table>";
 				$sql = $conn->prepare("SELECT * FROM questions WHERE sessionId = ? and resolved = 0 and announcement = 0 ORDER BY whenAsked ASC");
 				$sql->bind_param("s", $_SESSION["sessionId"]);
 				$sql->execute();
 				$result = $sql->get_result();
 				while($row = $result->fetch_assoc()){
-					$tableHTML .=  "<tr>";
-					$tableHTML .=  "<td>" . $row['studentName'] . "</td>";
-					$tableHTML .=  "<td>" . $row['questionData'] . "</td>";
-					$tableHTML .= "<td><form method=\"post\" action=\"taSession.php\"><input type=\"hidden\" name=\"resolveID\" value=\"" . $row['questionId'] ."\"><input type=\"submit\" name=\"resolve\" value=\"Resolve\"></form></td>";
+					$tableHTML .=  "<tr class=\"message\">";
+					$tableHTML .=  "<td class='mname'>" . $row['studentName'] . "</td>";
+					$tableHTML .=  "<td class='mquestion'>" . $row['questionData'] . "</td>";
+					$tableHTML .= "<td><form method=\"post\" action=\"taSession.php\"><input type=\"hidden\" name=\"resolveID\" value=\"" . $row['questionId'] ."\"><input class=\"resolve\" type=\"submit\" name=\"resolve\" value=\"\"></form></td>";
 					$tableHTML .=  "</tr>";
 				}
 				$tableHTML .= "</table>
 					</div>";
 				if($result->num_rows == 0)
-					$tableHTML = "</br>There are no questions in this session yet";
+					$tableHTML = "";
 				return $tableHTML;
 			}
 
 			function displayAnnouncements()
 			{
 				global $conn;
-				$tableHTML = "<div class=\"announcements\" style=\"padding:20px\" >
-						<table border=\"1\" style=\"max-width:900px;\">
+				$tableHTML = "<div class=\"announcements\">
+						<table >
 							<tr>
 								<th>Time</th>
 								<th>Announcement</th>
@@ -121,7 +108,7 @@ echo "Connected successfully";
 				$tableHTML .= "</table>
 					</div>";
 				if($result->num_rows == 0)
-					$tableHTML = "</br>You have yet to make an announcement.</br>";
+					$tableHTML = "You have yet to make an announcement.</br>";
 				return $tableHTML;
 			}
 
@@ -150,6 +137,11 @@ echo "Connected successfully";
 			}
 
 		?>
-
+		<div class="insertAnnouncement">
+			<form method="post" action="taSession.php">
+				<textarea rows="5" cols="60" id="announcementData" name="announcementData" placeholder="Insert Announcement"></textarea>
+				<input type="submit" id="Submit" value="Announce"/>
+			</form>
+		</div>
 	</body>
 </html>
